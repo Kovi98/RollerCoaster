46ae55c1857ba36ec4797005cb5039483f7c0be3
Jan Pekař <pekar@asd-software.cz>
2021-05-18 08:19:21 +0200

    Add project files.

---
+++ RollerCoaster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RollerCoaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
